8877bad9bf2682fff9ed198ffd56f184f72aab78
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-01-22 13:48:16 +0900

    공통 헤더 인증필터 모듈화

---
+++ Card-Service/src/main/kotlin/com/hyunbindev/cardservice/CardServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.hyunbindev.cardservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CardServiceApplication>(*args)
+}
---
+++ Gate-Way/src/main/kotlin/com/hyunbindev/gateway/GateWayApplication.kt
@@ -0,0 +1,11 @@
+package com.hyunbindev.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GateWayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GateWayApplication>(*args)
+}
