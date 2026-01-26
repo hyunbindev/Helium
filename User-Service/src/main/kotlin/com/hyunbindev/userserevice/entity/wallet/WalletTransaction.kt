df66d39647f38f3b596f9f906df710ec2e74e6f9
brutalKim <132333588+brutalKim@users.noreply.github.com>
2026-01-26 12:48:50 +0900

    wallet Transacttion fetch type lazy

--- User-Service/src/main/kotlin/com/hyunbindev/userserevice/entity/wallet/WalletTransaction.kt
+++ User-Service/src/main/kotlin/com/hyunbindev/userserevice/entity/wallet/WalletTransaction.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -18,7 +19,7 @@ import java.util.UUID
  */
 @Entity
 class WalletTransaction(
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     val wallet: WalletEntity,
 
     @Column(nullable = false)
